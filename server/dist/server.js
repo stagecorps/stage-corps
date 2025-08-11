efec15a6f63ee65a9301e301dcfb9ec266c95208
josh-hensley <me@joshhensley.com>
2025-08-11 12:39:46 -0500

    cleaning up files

--- server/dist/server.js
+++ server/dist/server.js
@@ -5,12 +5,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
-const api_1 = __importDefault(require("./routes/api"));
+const api_js_1 = __importDefault(require("./routes/api.js"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 3000;
 app.use('/static', express_1.default.static('../../client/dist'));
-app.use('/api', api_1.default);
+app.use('/api', api_js_1.default);
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });

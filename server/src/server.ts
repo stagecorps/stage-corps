1b6ad4994702a3ccd1f008fcc79988d426d4a09a
josh-hensley <me@joshhensley.com>
2025-08-11 13:55:20 -0500

    fixed serving bug

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,21 +1,29 @@
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import path from 'path';
-// import apiRoutes from './routes/api.js';
+import apiRoutes from './routes/api.js';
 
 dotenv.config()
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use('/static', express.static(path.join(__dirname, '../../client/dist')))
+app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-// app.use('/api', apiRoutes)
+app.use(express.static(path.join(__dirname, '../../client/dist')))
+app.use('/api', apiRoutes)
 
-app.get(/(.*)/, (_req: Request, res: Response)=>{
+app.get(/(.*)/, (_req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, '../../client/dist', 'index.html'));
 })
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+if (process.exitCode === 0) {
+  server.close(() => {
+    console.log('Server has stopped listening.')
+  })
+}
+
